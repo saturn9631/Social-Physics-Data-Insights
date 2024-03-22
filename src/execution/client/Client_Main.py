93e157a94a4aa2928bd58277619c9d277ce7ad90
Shakari Wade <saturn.kari.9@gmail.com>
2024-03-21 20:10:31 -0700

    Moved and added files to restructure into a client server model, and romeved the spreadsheet file and the accompaning script.

---
+++ src/execution/client/Client_Main.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import scipy as sc
+import sympy as smp
+import networkx as nx
+import torch
+
+def main ():
+    pass
+
+class PDR_Client:
+    def __init__(self):
+        pass
+    def run (self):
+        pass
+    def __del__(self):
+        pass
+
+if __name__ == "__main__":
+    main()
---
+++ src/execution/server/Server_Main.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+import scipy as sc
+import sympy as smp
+import networkx as nx
+import torch
+
+def main ():
+    pass
+
+class PDR_Server:
+    def __init__ (self):
+        #Fill in logic to find data for the fields
+        #self.dataBase = sql
+        #self.data = pd.DataFrame({})
+        pass
+    def update (self):
+        pass
+    def __del__(self):
+        pass
+
+if __name__ == "__main__":
+    main()
