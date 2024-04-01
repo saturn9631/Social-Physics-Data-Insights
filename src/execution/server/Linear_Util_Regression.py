34f5dcc41fe1ce5c71f1b3d43dca7452cbd481ba
Shakari Wade <saturn.kari.9@gmail.com>
2024-04-01 00:06:54 -0700

    This commit does what the first commit was supposed to do. For some reason the files were not commit in the last commit.

--- src/execution/server/Linear_Util_Regression.py
+++ src/execution/server/Linear_Util_Regression.py
@@ -2,10 +2,10 @@ import pandas as pd
 import numpy as np
 import sympy as smp
 import networkx as nx
+import torch
 
 import sys
 sys.path.append("lib/")
 sys.path.append("test/")
 
-strategies, payoffs = smp.symbols("sigma u")
 
