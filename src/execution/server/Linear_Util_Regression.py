f640e543a929c9a6b469d54ea7078b79b65013fb
Shakari Wade <saturn.kari.9@gmail.com>
2024-04-06 13:20:07 -0700

    Linear_Util_Regression.py will generate a differential equation of the utility by using regression on a Taylor Series, eliminating the insignifficant terms and setting the terms equal to each other; part of the implement code is written in this commmit.

--- src/execution/server/Linear_Util_Regression.py
+++ src/execution/server/Linear_Util_Regression.py
@@ -3,6 +3,8 @@ import numpy as np
 import sympy as smp
 import networkx as nx
 import torch
+import numba as nb
+from numba import cuda
 
 import sys
 sys.path.append("lib/")
@@ -10,4 +12,14 @@ sys.path.append("test/")
 
 import Actor_Tensor
 
+def taylor_series_regression (accuracy):
+    taylor_coefficients_matrix = np.zeros(accuracy)
 
+@cuda.jit
+def regress():
+    pass
+
+def convert_taylor_to_diff_eq():
+    pass
+
+#Write code to fit a taylor series to the data and then use that generate a differential equation by eliminating insignificant terms and setting the remaing terms equal to each other.
