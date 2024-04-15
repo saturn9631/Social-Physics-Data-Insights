449e550bd0ca8827f14aafe6f3d14afd29554186
Shakari Wade <saturn.kari.9@gmail.com>
2024-04-15 01:33:52 -0700

    Packaged methods into a more relevent class, started pseudocode for finding the best fit Taylor's series through regression, and working out the algebra for turning the resulting Taylor's series into a differential equation in pseudocode; with the actual code implementing an algorithm derived from the algebra.

--- src/execution/server/Linear_Util_Regression.py
+++ src/execution/server/Linear_Util_Regression.py
@@ -1,3 +1,4 @@
+import typing
 import pandas as pd
 import numpy as np
 import sympy as smp
@@ -12,14 +13,34 @@ sys.path.append("test/")
 
 import Actor_Tensor
 
-def taylor_series_regression (accuracy):
-    taylor_coefficients_matrix = np.zeros(accuracy)
+class Taylor_Model:
+    """Encapsulates a best fit model based on Taylor's series."""
+    def __init__(self, terms: int):
+        self.taylor_coefficients_matrix = np.zeros(accuracy)
 
-@cuda.jit
-def regress():
-    pass
+    def forward (input):
+        pass
+        """Pseudocode: f = taylor polynomial
+        return f(input)"""
+    
+    def backward (data : np.ndarray):
+        pass
 
-def convert_taylor_to_diff_eq():
-    pass
+    def regress(paramaters):
+        pass
 
-#Write code to fit a taylor series to the data and then use that generate a differential equation by eliminating insignificant terms and setting the remaing terms equal to each other.
+    def convert_to_diff_eq():
+        pass
+        """
+        Algebra: f(x) = f(a) + (f'(a)/1!)(x-a) + (f"(a)/2!)(x-a)^2 + (f"'(a)/3!)(x-a)^3 + (f""(a)/4!)(x-a)^4 + ...
+        f(x) =~ f(a) + (f'(a)/1!)(x-a) + (f"(a)/2!)(x-a)^2
+        f'(x) =~ f'(a) + f"(a)(x-a)
+        f"(x) =~ f"(a)
+        f'(x)-f'(a) =~ f"(a)(x-a)
+        (f'(x)-f'(a))/f"(a) =~ x-a or (f'(x)-f'(a))/(x-a) =~ f"(a)
+        f(x) =~ f(a) + f'(a)((f'(x)-f'(a))/f"(a)) + f"(a)(f'(x)-f'(a))/2f"(a)
+        f(x) =~ f(a) + f'(a)((f'(x)-f'(a))/f"(a)) + (f'(x)-f'(a))/2
+        f(x) =~ 
+        Pseudocode: y(0)"""
+
+
