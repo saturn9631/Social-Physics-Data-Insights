ae2e0259a9d9882d313536fa274a3f56b6602a7c
Shakari Wade <saturn.kari.9@gmail.com>
2024-04-16 00:25:57 -0700

    Still working on the Taylor's series math.

--- src/execution/server/Linear_Util_Regression.py
+++ src/execution/server/Linear_Util_Regression.py
@@ -3,9 +3,10 @@ import pandas as pd
 import numpy as np
 import sympy as smp
 import networkx as nx
-import torch
 import numba as nb
 from numba import cuda
+import numba
+import math
 
 import sys
 sys.path.append("lib/")
@@ -18,8 +19,13 @@ class Taylor_Model:
     def __init__(self, terms: int):
         self.taylor_coefficients_matrix = np.zeros(accuracy)
 
-    def forward (input):
-        pass
+    def forward (self, input):
+        result = 0
+        index = 0
+        for coefficient in self.taylor_coefficients_matrix:
+            result = (coefficient / math.factorial(index)) * (input) ** index
+            index += 1
+        return result
         """Pseudocode: f = taylor polynomial
         return f(input)"""
     
@@ -32,15 +38,15 @@ class Taylor_Model:
     def convert_to_diff_eq():
         pass
         """
-        Algebra: f(x) = f(a) + (f'(a)/1!)(x-a) + (f"(a)/2!)(x-a)^2 + (f"'(a)/3!)(x-a)^3 + (f""(a)/4!)(x-a)^4 + ...
-        f(x) =~ f(a) + (f'(a)/1!)(x-a) + (f"(a)/2!)(x-a)^2
-        f'(x) =~ f'(a) + f"(a)(x-a)
+        Algebra: f(x) = f(a) + (f'(a)/1!)(x - a) + (f"(a)/2!)(x - a)^2 + (f"'(a)/3!)(x - a)^3 + (f""(a)/4!)(x - a)^4 + ...
+        f(x) =~ f(a) + (f'(a)/1!)(x - a) + (f"(a)/2!)(x - a)^2
+        f'(x) =~ f'(a) + f"(a)(x - a)
         f"(x) =~ f"(a)
-        f'(x)-f'(a) =~ f"(a)(x-a)
-        (f'(x)-f'(a))/f"(a) =~ x-a or (f'(x)-f'(a))/(x-a) =~ f"(a)
-        f(x) =~ f(a) + f'(a)((f'(x)-f'(a))/f"(a)) + f"(a)(f'(x)-f'(a))/2f"(a)
-        f(x) =~ f(a) + f'(a)((f'(x)-f'(a))/f"(a)) + (f'(x)-f'(a))/2
-        f(x) =~ 
+        f'(x) - f'(a) =~ f"(a)(x - a)
+        (f'(x) - f'(a))/f"(a) =~ x - a or (f'(x) - f'(a))/(x - a) =~ f"(a)
+        f(x) =~ f(a) + f'(a)((f'(x) - f'(a))/f"(a)) + f"(a)(f'(x) - f'(a))/2f"(a)
+        f(x) =~ f(a) + f'(a)((f'(x) - f'(a))/f"(a)) + (f'(x) - f'(a))/2
+        f(x) =~ f(a) + f'(a)f'(x)/f"(a) - (f'(a)^2)/f"(a)
         Pseudocode: y(0)"""
 
 
