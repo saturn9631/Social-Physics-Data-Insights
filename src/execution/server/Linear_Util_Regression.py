6b5c0372290f7c982ad0681a6ca0700b55802150
Shakari Wade <saturn.kari.9@gmail.com>
2024-04-21 17:13:52 -0700

    Added code, reorganized, documented methods.

--- src/execution/server/Linear_Util_Regression.py
+++ src/execution/server/Linear_Util_Regression.py
@@ -16,27 +16,49 @@ import Actor_Tensor
 
 class Taylor_Model:
     """Encapsulates a best fit model based on Taylor's series."""
-    def __init__(self, terms : int, center = 0 : int):
-        self.coefficients = np.zeros(accuracy)
+    def __init__(self, terms, center):
+        """Parameters: 
+        terms (int): How many terms does the Taylor polynomial have.
+        center (float): The value of the center of approximation."
+        """
+        self.coefficients = np.zeros(terms)
         self.center = center
 
-    def forward (self, input):
+    def evaluate (self, input):
+        """Returns the value of the Taylor polynomial at the \"input\" variable.
+        Parameters:
+        input (float): The value at which the Taylor polynomial is evaluated at.
+        Returns: The value of the Taylor polynomial at the input.
+        """
         result = 0
         index = 0
         for coefficient in self.coefficients:
-            result += (coefficient / math.factorial(index)) * (input - center) ** index
+            result += (coefficient / smp.factorial(index)) * (input - center) ** index
             index += 1
         return result
-        """Pseudocode: f = taylor polynomial
-        return f(input)"""
-    
-    def backward (data : np.ndarray):
+
+    #Cuda tag
+    def forward ():
+        pass
+        #Forward is supposed to evaluate across an array and use cuda
+
+    #Cuda tag
+    def backward (self, data : np.ndarray):
+        """"""
         pass
 
-    def regress(paramaters):
-        pass
+    def getTaylorPolynomial (self, variable = smp.symbols("x")):
+        """Returns the Taylor polynomial as a sympy object. E.g.: getTaylorPolynomial (sympy.symbols("s")) -> a + b(s-q) + c(s-q) + ..., where a, b, c are the Taylor coefficients, and q is the center.
+        Parameters:
+        variable (sympy Symbol): the input variable for the Taylor polynomial. 
+        Return:
+        (sympy Sum): Returns the Taylor polynomial with \"variable\" as the input."""
+        for count in range(terms):
+            result = (self.coefficients[count] / smp.factorial(count)) * (variable - self.center) ** count
+        return (result, variable)
 
-    def convert_to_diff_eq(order : int) -> smp.Eq:
+    def convert_to_diff_eq (self):
+        """"""
         pass
         """
         Pseudocode:
