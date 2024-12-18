3fa17e33d97d0e689852d29fc5080dd067858898
Shakari Wade <saturn.kari.9@gmail.com>
2024-12-17 23:20:10 -0800

    Cleaned up clutter.

---
+++ src/main.py
@@ -0,0 +1,20 @@
+#!shebang
+
+import typing
+#import multiprocessing
+
+#import pymysql
+import pandas as pd
+
+from data import *;
+from game import *;
+from institution import *;
+from psychology import *;
+from test import main_test
+
+def main ():
+
+
+if __name__ == "__main__":
+    main()
+
